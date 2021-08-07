e7df64ed921fa23a71d21b23096aeb7e06a19120
TinoMadrid <tinomadrid77@gmail.com>
2021-08-07 12:14:38 -0500

    Added csv with years of fire outbreaks.

---
+++ firePredictor.py
@@ -0,0 +1,10 @@
+from pandas import read_table
+import numpy as np
+import matplotlib.pyplot as plt
+
+def readData():
+    # Function reads csv file into memory
+    print("Read data here")
+
+if __name__ == '__main__':
+    print("Beginning")
