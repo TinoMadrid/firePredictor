2fbf59e962fdff0318e9635c49d94bc91303e7b7
TinoMadrid <tinomadrid77@gmail.com>
2021-08-12 20:05:40 -0500

    Added year variables to count occurrences of fires outbreaking in different regions of the US by 5 year increments from the dataset all years (1992-2015).

--- firePredictor.py
+++ firePredictor.py
@@ -11,27 +11,92 @@ def readDataIntoHistogram():
     southEast = ['AR', 'LA', 'MS', 'AL', 'TN', 'KY', 'WV', 'VA', 'NC', 'SC', 'GA', 'FL']
     northEast = ['ME', 'NH', 'VT', 'NY', 'PA', 'MD', 'DE', 'NJ', 'CT', 'RI', 'MA']
 
+    #keep count of years by increments of 5
+    twoTo1995 = ['1992', '1993', '1994', '1995']
+    sixTo2000 = ['1996', '1997', '1998', '1999', '2000']
+    oneTo2005 = ['2001', '2002', '2003', '2004', '2005']
+    sixTo2010 = ['2006', '2007', '2008', '2009', '2010']
+    elevenTo2015 = ['2011', '2012', '2013', '2014', '2015']
+
     #used to keep count of fires in each region
     w = sW = mW = sE = nE = 0
+
+    #used to keep count of fires in each region for that year
+    west1992To1995 = west1996To2000 = west2001To2005 = west2006To2010 = west2011To2015 = 0
+    southWest1992To1995 = southWest1996To2000 = southWest2001To2005 = southWest2006To2010 = southWest2011To2015 = 0
+    midWest1992To1995 = midWest1996To2000 = midWest2001To2005 = midWest2006To2010 = midWest2011To2015 = 0
+    southEast1992To1995 = southEast1996To2000 = southEast2001To2005 = southEast2006To2010 = southEast2011To2015 = 0
+    northEast1992To1995 = northEast1996To2000 = northEast2001To2005 = northEast2006To2010 = northEast2011To2015 = 0
 
     resultRows = []
     with open('fireData.csv', 'r') as readOBJ:
         csvReader = reader(readOBJ)
         resultRows.append(list(csvReader))
+        print(resultRows)
 
-
+    #count occurrences of each region and years
     for rows in resultRows:
         for row in rows:
             if row[1] in west:
                 w += 1
+                if row[3] in twoTo1995:
+                    west1992To1995 += 1
+                elif row[3] in sixTo2000:
+                    west1996To2000 += 1
+                elif row[3] in oneTo2005:
+                    west2001To2005 += 1
+                elif row[3] in sixTo2010:
+                    west2006To2010 += 1
+                elif row[3] in elevenTo2015:
+                    west2011To2015 += 1
             elif row[1] in southWest:
                 sW += 1
+                if row[3] in twoTo1995:
+                    southWest1992To1995 += 1
+                elif row[3] in sixTo2000:
+                    southWest1996To2000 += 1
+                elif row[3] in oneTo2005:
+                    southWest2001To2005 += 1
+                elif row[3] in sixTo2010:
+                    southWest2006To2010 += 1
+                elif row[3] in elevenTo2015:
+                    southWest2011To2015 += 1
             elif row[1] in midWest:
                 mW += 1
+                if row[3] in twoTo1995:
+                    midWest1992To1995 += 1
+                elif row[3] in sixTo2000:
+                    midWest1996To2000 += 1
+                elif row[3] in oneTo2005:
+                    midWest2001To2005 += 1
+                elif row[3] in sixTo2010:
+                    midWest2006To2010 += 1
+                elif row[3] in elevenTo2015:
+                    midWest2011To2015 += 1
             elif row[1] in southEast:
                 sE += 1
+                if row[3] in twoTo1995:
+                    southEast1992To1995 += 1
+                elif row[3] in sixTo2000:
+                    southEast1996To2000 += 1
+                elif row[3] in oneTo2005:
+                    southEast2001To2005 += 1
+                elif row[3] in sixTo2010:
+                    southEast2006To2010 += 1
+                elif row[3] in elevenTo2015:
+                    southEast2011To2015 += 1
             elif row[1] in northEast:
                 nE += 1
+                if row[3] in twoTo1995:
+                    northEast1992To1995 += 1
+                elif row[3] in sixTo2000:
+                    northEast1996To2000 += 1
+                elif row[3] in oneTo2005:
+                    northEast2001To2005 += 1
+                elif row[3] in sixTo2010:
+                    northEast2006To2010 += 1
+                elif row[3] in elevenTo2015:
+                    northEast2011To2015 += 1
             else:
                 x = 0
     plotHistogram(w, sW, mW, sE, nE)
@@ -40,9 +105,19 @@ def plotHistogram(west, southwest, midwest, southeast, northeast):
     valueList = [west, southwest, midwest, southeast, northeast]
 
     xAxisLabels = ['West', 'SouthWest', 'MidWest', 'SouthEast', 'NorthEast']
+    X = np.arange(4)
+    fig = plt.figure()
+    ax = fig.add_axes([0,0,1,1])
+
+    #ax.bar(X + 0.00, data[0], color='b', width=0.25)
+    #ax.bar(X + 0.25, data[1], color='g', width=0.25)
+    #ax.bar(X + 0.50, data[2], color='r', width=0.25)
 
     plt.bar(xAxisLabels, valueList)
     plt.show()
+
+def generatePredictionData(frame):
+    print(frame)
 
 if __name__ == '__main__':
     print("Beginning")
